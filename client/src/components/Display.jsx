fbce688b7c773fd41ac92a43363ce0e3de345dfc
el-noir <mudasirshah9777@gmail.com>
2025-04-29 01:29:50 +0500

    ff

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -7,34 +7,37 @@ const Display = ({ contract, account }) => {
     let dataArray;
     const otherAddress = document.querySelector(".address").value;
 
+   try {
     if (otherAddress) {
-      dataArray = await contract.display(otherAddress);
-    } else {
-      dataArray = await contract.display(account);
-    }
-
-    if (dataArray.length > 0) {
-      const images = dataArray.map((item, i) => {
-        let cid = item;
-        // Remove 'ipfs://' prefix if present
-        if (cid.startsWith("ipfs://")) {
-          cid = cid.slice(7);
-        }
-        // Remove any suffix after colon (e.g., ':1')
-        if (cid.includes(":")) {
-          cid = cid.split(":")[0];
-        }
-        const gatewayUrl = `https://ipfs.io/ipfs/${cid}`;
-        return (
-          <a href={gatewayUrl} key={i} target="_blank" rel="noopener noreferrer">
-            <img src={gatewayUrl} alt="Uploaded content" className="image-list" />
-          </a>
-        );
-      });
-      setData(images);
-    } else {
-      alert("No images to display");
-    }
+        dataArray = await contract.display(otherAddress);
+      } else {
+        dataArray = await contract.display(account);
+      }
+   } catch (error) {
+    alert("You dont have access");
+   }
+   if (dataArray.length > 0) {
+    const images = dataArray.map((item, i) => {
+      let cid = item;
+      // Remove 'ipfs://' prefix if present
+      if (cid.startsWith("ipfs://")) {
+        cid = cid.slice(7);
+      }
+      // Remove any suffix after colon (e.g., ':1')
+      if (cid.includes(":")) {
+        cid = cid.split(":")[0];
+      }
+      const gatewayUrl = `https://ipfs.io/ipfs/${cid}`;
+      return (
+        <a href={gatewayUrl} key={i} target="_blank" rel="noopener noreferrer">
+          <img src={gatewayUrl} alt="Uploaded content" className="image-list" />
+        </a>
+      );
+    });
+    setData(images);
+  } else {
+    alert("No images to display");
+  }
   };
 
   return (
