0eca442707ad4abc8cb91922a5b7d544c0533de6
el-noir <mudasirshah9777@gmail.com>
2025-04-28 22:08:06 +0500

    Retrieve file + Upload to Blockchain Success

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -18,8 +18,9 @@ const FileUpload =({contract, account, provider})=>{
               url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
               data: formData,
               headers: {
-                pinata_api_key: import.meta.env.VITE_PINATA_API_KEY,
-                pinata_secret_api_key: import.meta.env.VITE_PINATA_SECRET_API_KEY,
+                'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI1ZjU2ZTA0Mi0wMTNhLTQ0YjAtYmMxOC0yODQ3YmI4YTE3ZTEiLCJlbWFpbCI6Im11ZGFzaXJzaGFoOTc3N0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiOGUzN2IwMzk3MWE4YzRmY2E1NDciLCJzY29wZWRLZXlTZWNyZXQiOiJlYzdkN2M2OWM3MzJkYzU0ZTBkOGEzNGJhY2UxZDJlZDMyMTY5NTQ3ZDU0NWFiYjFmMTg4YWU2NWFhMzE1NzlkIiwiZXhwIjoxNzc3Mzk1NzcxfQ.utH4Av4DIeZbi3I0mAjeMSg4j0gQisoYsWYvdNZF6qI`,
+                pinata_api_key: `8e37b03971a8c4fca547`,
+                pinata_secret_api_key: `ec7d7c69c732dc54e0d8a34bace1d2ed32169547d545abb1f188ae65aa31579d`,
                 "Content-Type": "multipart/form-data",
               },
             });
@@ -43,7 +44,15 @@ const FileUpload =({contract, account, provider})=>{
     
 
     const retrieveFile = (event) => {
-
+     const data = event.target.files[0];
+     console.log(data);
+     const reader = new window.FileReader();
+     reader.readAsArrayBuffer(data);
+     reader.onloadend=()=>{
+        setFile(event.target.files[0]);
+     }
+     setFileName(event.target.files[0].name);
+     event.preventDefault();
     };
 
     return( <div>
@@ -53,7 +62,7 @@ const FileUpload =({contract, account, provider})=>{
         </label>
         <input type="file" disabled={!account} id="file-upload" name="data" onChange={retrieveFile} />
         <span className="textArea">Image: {fileName}</span>
-        <button type="submit" className="upload">Upload</button>
+        <button type="submit" className="upload" disabled={!file}>Upload</button>
        </form>
     </div>
     )
